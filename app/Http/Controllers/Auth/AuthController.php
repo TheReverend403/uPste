af413f8394fbcf519ef8ec01dae6d620129100aa
Lee Watson <rev@revthefox.co.uk>
2015-12-09 23:33:07 +0000

    Require alphanumeric names only

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -46,7 +46,7 @@ class AuthController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => 'required|max:255|unique:users',
+            'name' => 'required|max:255|unique:users|alpha_num',
             'email' => 'required|email|max:255|unique:users',
             'password' => 'required|confirmed|min:6',
         ]);
