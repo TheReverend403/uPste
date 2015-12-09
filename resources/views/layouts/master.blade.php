14228be5b290c2af15ce6554bd285799e95d9c74
Lee Watson <rev@revthefox.co.uk>
2015-12-09 19:47:14 +0000

    Fix nav collapse

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -16,7 +16,7 @@
         <div class="container">
             <!-- Brand and toggle get grouped for better mobile display -->
             <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#main-nav" aria-expanded="false">
                     <span class="sr-only">Toggle navigation</span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
@@ -26,7 +26,7 @@
             </div>
 
             <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse">
+            <div class="collapse navbar-collapse" id="main-nav">
                 <ul class="nav navbar-nav">
                     @yield('nav-left')
                 </ul>
