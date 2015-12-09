d244708d7c8f2538aa3908ac51d617d5e13c153d
Lee Watson <rev@revthefox.co.uk>
2015-12-09 19:40:50 +0000

    Mobile friendly meta vars

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -2,6 +2,8 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" type="image/png" href="{{ url('img/favicon.png') }}">
     <title>@yield('title')</title>
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
