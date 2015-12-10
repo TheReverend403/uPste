670a384935405bc77a193bc26a0f7f130cc3f284
Lee Watson <rev@revthefox.co.uk>
2015-12-10 22:38:41 +0000

    Clarify file size limits

--- resources/views/account/new-member.blade.php
+++ resources/views/account/new-member.blade.php
@@ -4,7 +4,7 @@
         <li>For support, email <a href="mailto:{{ env('OWNER_EMAIL') }}">{{ env('OWNER_EMAIL') }}</a>.</li>
         <li>For support or social, hang out in {{ env('IRC_CHANNEL') }} on {{ env('IRC_SERVER') }}.</li>
         <li>While only members can upload files, all uploads are visible to the public if they know (or accidentally find) the URL. Therefore, <b>DO NOT</b> upload anything you consider private as we will not accept any responsibility if it gets leaked.</li>
-        <li>Upload limit is {{ env('UPLOAD_LIMIT') }}.</li>
+        <li>Max file size per upload is {{ env('UPLOAD_LIMIT') }}.</li>
         <li>Do not upload child porn or malware, you'll be banned without mercy.</li>
         <li>Some extra stuff is provided for members, <a href="{{ route('account.resources') }}">{{ route('account.resources') }}</a>.</li>
     </ol>
