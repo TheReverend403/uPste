b1a88aae10b84e6369dc3d6cf2b4229e1f93bf44
Lee Watson <rev@revthefox.co.uk>
2015-12-10 22:36:32 +0000

    Fix new member info to not be centered

--- resources/views/account/new-member.blade.php
+++ resources/views/account/new-member.blade.php
@@ -1,9 +1,9 @@
 <div class="well">
     <p><b>Welcome to {{ env('DOMAIN') }}, {{ Auth::user()->name }}. Here's everything you need to know.</b></p>
-    <ol>
+    <ol class="text-left">
         <li>For support, email <a href="mailto:{{ env('OWNER_EMAIL') }}">{{ env('OWNER_EMAIL') }}</a>.</li>
         <li>For support or social, hang out in {{ env('IRC_CHANNEL') }} on {{ env('IRC_SERVER') }}.</li>
-        <li>All uploads are visible to the public. Only members may upload.</li>
+        <li>While only members can upload files, all uploads are visible to the public if they know (or accidentally find) the URL. Therefore, <b>DO NOT</b> upload anything you consider private as we will not accept any responsibility if it gets leaked.</li>
         <li>Upload limit is {{ env('UPLOAD_LIMIT') }}.</li>
         <li>Do not upload child porn or malware, you'll be banned without mercy.</li>
         <li>Some extra stuff is provided for members, <a href="{{ route('account.resources') }}">{{ route('account.resources') }}</a>.</li>
