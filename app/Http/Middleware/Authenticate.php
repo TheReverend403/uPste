e54663ce0c2b5e048e67528bae07f3066f7e1357
Lee Watson <rev@revthefox.co.uk>
2015-12-10 02:01:09 +0000

    Notify an unauthenticated user when they need to be logged in

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -19,7 +19,7 @@ class Authenticate
     /**
      * Create a new filter instance.
      *
-     * @param  Guard  $auth
+     * @param  Guard $auth
      * @return void
      */
     public function __construct(Guard $auth)
@@ -30,8 +30,8 @@ class Authenticate
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
      * @return mixed
      */
     public function handle($request, Closure $next)
@@ -40,9 +40,12 @@ class Authenticate
             if ($request->ajax()) {
                 return response('Unauthorized.', 401);
             } else {
+                Session::flash('alert',
+                    'You must log in to access that page.');
                 return redirect()->route('login');
             }
         }
+        
         if (!Auth::user()->enabled) {
             Session::flash('alert',
                 'Your account has not been approved. You will be notified via email when your account status changes.');
