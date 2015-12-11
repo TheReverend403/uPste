d9fb76a4657f5bd56f0bcc429ee4bee1f5dd18e5
Lee Watson <rev@revthefox.co.uk>
2015-12-11 14:57:12 +0000

    Apparently error pages don't support blade

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,4 +1,19 @@
-@section('title', "404 - Page Not Found")
-@section('content')
-    <p>We couldn't find the page you were looking for.</p>
-@stop
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>404 - Page Not Found</title>
+    <link rel="icon" type="image/png" href="{{ url('img/favicon.png') }}">
+    <link rel="stylesheet" href="{{ url('css/error.css') }}">
+    <link href="//fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+</head>
+<body>
+<div class="container">
+    <div class="content">
+        <p>We couldn't find the page you were looking for.</p>
+    </div>
+</div>
+</body>
+</html>
--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,4 +1,18 @@
-@section('title', "We'll be right back.")
-@section('content')
-    <p>We'll be right back.</p>
-@stop
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>We'll be right back.</title>
+    <link rel="icon" type="image/png" href="{{ url('img/favicon.png') }}">
+    <link rel="stylesheet" href="{{ url('css/error.css') }}" type="text/css">
+</head>
+<body>
+<div class="container">
+    <div class="content">
+        <p>We'll be right back.</p>
+    </div>
+</div>
+</body>
+</html>
