e26156d4dd0ce44ecedd814c7bdbe6a522175e57
Lee Watson <rev@revthefox.co.uk>
2015-12-11 14:47:22 +0000

    Make error pages mobile frienly

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,6 +1,10 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
     <title>We'll be right back.</title>
     <link rel="icon" type="image/png" href="{{ url('img/favicon.png') }}">
 
