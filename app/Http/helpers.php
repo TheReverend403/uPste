8514154e57eeb8437610635037855ac9c119e36e
Lee Watson <rev@revthefox.co.uk>
2015-12-15 06:52:54 +0000

    Use the more accurate format_bytes()

--- app/Http/helpers.php
+++ app/Http/helpers.php
@@ -6,10 +6,7 @@ function format_bytes($bytes, $precision = 2)
     $bytes = max($bytes, 0);
     $pow = floor(($bytes ? log($bytes) : 0) / log(1024));
     $pow = min($pow, count($units) - 1);
-
-    // Uncomment one of the following alternatives
-    // $bytes /= pow(1024, $pow);
-    $bytes /= (1 << (10 * $pow));
-
+    $bytes /= pow(1024, $pow);
+    
     return round($bytes, $precision) . ' ' . $units[$pow];
 }
