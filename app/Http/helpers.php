abaaac1212321ff39868d1e4a1ed1dbc9ca7b472
Lee Watson <rev@revthefox.co.uk>
2015-12-15 07:00:13 +0000

    Use the actual size for our units

--- app/Http/helpers.php
+++ app/Http/helpers.php
@@ -1,12 +1,14 @@
 <?php
+
+// http://stackoverflow.com/questions/2510434/format-bytes-to-kilobytes-megabytes-gigabytes
 function format_bytes($bytes, $precision = 2)
 {
-    $units = array('B', 'KB', 'MB', 'GB', 'TB');
+    $units = ['B', 'KB', 'MB', 'GB', 'TB'];
 
     $bytes = max($bytes, 0);
-    $pow = floor(($bytes ? log($bytes) : 0) / log(1024));
+    $pow = floor(($bytes ? log($bytes) : 0) / log(1000));
     $pow = min($pow, count($units) - 1);
-    $bytes /= pow(1024, $pow);
-    
+    $bytes /= pow(1000, $pow);
+
     return round($bytes, $precision) . ' ' . $units[$pow];
 }
