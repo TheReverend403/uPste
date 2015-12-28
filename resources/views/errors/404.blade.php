137156276a5e254892d398ae5b6e3070a53fed8e
Lee Watson <rev@revthefox.co.uk>
2015-12-28 11:57:37 +0000

    Fix error page urls

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -5,8 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>404 - Page Not Found</title>
-    <link rel="icon" type="image/png" href="{{ url('img/favicon.png') }}">
-    <link rel="stylesheet" href="{{ url('css/error.css') }}">
+    <link rel="icon" type="image/png" href="{{ url('assets/img/favicon.png') }}">
+    <link rel="stylesheet" href="{{ elixir('assets/css/error.css') }}">
     <link href="//fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
 </head>
 <body>
--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -5,8 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>We'll be right back.</title>
-    <link rel="icon" type="image/png" href="{{ url('img/favicon.png') }}">
-    <link rel="stylesheet" href="{{ url('css/error.css') }}" type="text/css">
+    <link rel="icon" type="image/png" href="{{ url('assets/img/favicon.png') }}">
+    <link rel="stylesheet" href="{{ elixir('assets/css/error.css') }}">
 </head>
 <body>
 <div class="container">
