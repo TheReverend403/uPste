4e8e00808912a9b93390ca71776ff53fec09d83f
Lee Watson <rev@revthefox.co.uk>
2016-01-21 02:17:40 +0000

    I hate writing code on a tablet

--- config/database.php
+++ config/database.php
@@ -59,7 +59,7 @@ return [
             'username'  => env('DB_USERNAME', 'forge'),
             'password'  => env('DB_PASSWORD', ''),
             'charset'   => 'utf8',
-            'collation' => 'utf8_bin,
+            'collation' => 'utf8_bin',
             'prefix'    => '',
             'strict'    => false,
         ],
