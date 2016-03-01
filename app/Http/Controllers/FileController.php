01df526f0006c007c4eec1b75aadb30a8e1ee5e7
Lee Watson <rev@revthefox.co.uk>
2016-03-01 23:59:57 +0000

    Revert "nginx pls"
    This reverts commit e3a260bb0aa9c4407d5ec416d1a289886c158639.

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -14,7 +14,7 @@ class FileController extends Controller
     public function index(Upload $upload)
     {
         if (Storage::exists('uploads/' . $upload->name)) {
-            response()->make(null, StatusCode::OK, ['X-Accel-Redirect' => Storage::getDriver()->getAdapter()->getPathPrefix() . '/' . $upload->name]);
+            response()->make('', StatusCode::OK, ['X-SendFile' => Storage::getDriver()->getAdapter()->getPathPrefix() . '/' . $upload->name]);
         } else {
             throw new NotFoundHttpException();
         }
