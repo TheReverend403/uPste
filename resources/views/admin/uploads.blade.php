dc2829f26bd8e2434bc99804aae71c0bd23316a9
Lee Watson <rev@revthefox.co.uk>
2016-04-04 19:53:06 +0100

    Handle page resizing better

--- resources/views/admin/uploads.blade.php
+++ resources/views/admin/uploads.blade.php
@@ -24,8 +24,9 @@
                                     <div class="media-body">
                                         <div class="col-lg-10">
                                             <h4 class="media-heading visible-lg">{{ str_limit($upload->original_name, 30) }}</h4>
-                                            <h4 class="media-heading visible-md">{{ str_limit($upload->original_name, 20) }}</h4>
-                                            <h4 class="media-heading visible-xs visible-sm">{{ str_limit($upload->original_name, 10) }}</h4>
+                                            <h4 class="media-heading visible-md">{{ str_limit($upload->original_name, 25) }}</h4>
+                                            <h4 class="media-heading visible-sm">{{ str_limit($upload->original_name, 20) }}</h4>
+                                            <h4 class="media-heading visible-xs">{{ str_limit($upload->original_name, 15) }}</h4>
                                             <h5 class="visible-xs visible-sm"><b>URL:</b> <a href="{{ route('files.get', $upload) }}">{{ route('files.get', $upload) }}</a></h5>
                                             <h5><b>Size:</b> {{ App\Helpers::formatBytes($upload->size) }}</h5>
                                             <h5><b>Views:</b> {{ $upload->views }}</h5>
