e4dad3a0339c558e474104f19576588700456a79
Lee Watson <rev@revthefox.co.uk>
2016-07-03 10:09:03 +0100

    Add svg to nginx config to be served as plaintext due to https://hackerone.com/reports/148853

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -3,9 +3,16 @@
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use App\Helpers;
+use App\Models\User;
 
 class ExampleTest extends TestCase
 {
+    public function setUp() {
+        parent::setUp();
+        Artisan::call('migrate');
+    }
+
     /**
      * A basic functional test example.
      *
@@ -13,7 +20,13 @@ class ExampleTest extends TestCase
      */
     public function testBasicExample()
     {
-        $this->visit('/')
-             ->see('Laravel 5');
+        User::create([
+            'name' => 'TestUser',
+            'email' => 'test@test.com',
+            'apikey' => str_random(Helpers::API_KEY_LENGTH),
+            'password' => Hash::make(str_random(16), ['rounds' => config('upste.password_hash_rounds')]),
+        ]);
+
+        $this->assertTrue(User::all()->count() == 1);
     }
 }
