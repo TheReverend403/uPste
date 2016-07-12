a114bc401685aeeb2781fa5df02ebe997f24de49
Lee Watson <rev@revthefox.co.uk>
2016-07-12 17:07:10 +0100

    Allow better configuration of redis

--- config/database.php
+++ config/database.php
@@ -116,9 +116,11 @@ return [
         'cluster' => false,
 
         'default' => [
-            'host'     => '127.0.0.1',
-            'port'     => 6379,
-            'database' => 0,
+            'host'     => env('REDIS_HOST', '127.0.0.1'),
+            'path'     => env('REDIS_PATH', '/tmp/redis.sock'),
+            'port'     => env('REDIS_PORT', 6379),
+            'database' => env('REDIS_DATABASE', 0),
+            'scheme'   => env('REDIS_SCHEME', 'tcp')
         ],
 
     ],
